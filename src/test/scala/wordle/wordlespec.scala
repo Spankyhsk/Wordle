24cdd01d464c4a284705b930ba882a2add203de0
steffhaa <steffen.haas@htwg-konstanz.de>
2023-10-24 17:15:03 +0200

    Aufteilung Klassen CommitFenster

--- src/test/scala/wordle/wordlespec.scala
+++ src/test/scala/wordle/wordlespec.scala
@@ -4,7 +4,22 @@ import org.scalatest.matchers.should.Matchers._
 
 
 
-class wordlespec {
+class wordlespec extends AnyWordSpec {
+
+  "Wordle" should {
+    
+
+      val array1Buchstab: Array[String] = Array("A", "B", "C", "D")
+      val loesungsword = "B"
+
+    "have an array and a solution word" in {
+      array1Buchstab.mkString(", ") should be ("A, B, C, D")  //.mkString(", ") --> "A, B, C, D"
+      loesungsword should be("B")
+    }
+
+    }
+
+
 
 
 }
