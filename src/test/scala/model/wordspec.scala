8bb831af2b67955a7e31ba1c693b3b5ca159878a
Konstantin Göke <146178432+Spankyhsk@users.noreply.github.com>
2023-11-01 18:06:59 +0100

    wordspec

--- src/test/scala/model/wordspec.scala
+++ src/test/scala/model/wordspec.scala
@@ -1,5 +1,21 @@
 package model
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
 
-class wordspec {
+class wordspec extends AnyWordSpec with Matchers{
+  "word" should{
+    "select a random word" in {
+      val wordsByLength = Map(
+        1 -> Array("a", "b", "c", "d", "e"),
+        2 -> Array("ab", "bc", "cd", "de", "ef"),
+        3 -> Array("abc", "bcd", "cde", "def", "efg"),
+        4 -> Array("abcd", "bcde", "cdef", "defg", "efgh"),
+        5 -> Array("abcde", "bcdef", "cdefg", "defgh", "efghi")
+      )
+
+      val word = wordle.selectRandomWord(wordsByLength(3))
+      wordsByLength(3) should contain(word)
+    }
+  }
 
 }
