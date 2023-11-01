4ba3ff4c719db74b6c5147cdc252062f66953013
Konstantin Göke <146178432+Spankyhsk@users.noreply.github.com>
2023-11-01 17:41:52 +0100

    val attempt = new attepemt

--- src/test/scala/model/attemptspec.scala
+++ src/test/scala/model/attemptspec.scala
@@ -6,7 +6,7 @@ import org.scalatest.wordspec.AnyWordSpec
 class attemptspec extends AnyWordSpec{
   "A attempt" should{
     "have a target, and a x attempt," in{
-      val attempt = attempt("fisch", 1)
+      val attempt = new attempt("fisch", 1)
       attempt.targetword should be("fisch")
       attempt.x should be(1)
     }
