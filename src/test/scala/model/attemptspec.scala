0e30571a9db704eb4c8c17b79b1267bc57d63498
Konstantin Göke <146178432+Spankyhsk@users.noreply.github.com>
2023-11-01 17:31:35 +0100

    attemptspec gemacht

--- src/test/scala/model/attemptspec.scala
+++ src/test/scala/model/attemptspec.scala
@@ -5,6 +5,10 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class attemptspec extends AnyWordSpec{
   "A attempt" should{
-    ""
+    "have a target, and a x attempt," in{
+      val attempt = attempt("fisch", 1)
+      attempt.targetword should be("fisch")
+      attempt.x should be(1)
+    }
   }
 }
