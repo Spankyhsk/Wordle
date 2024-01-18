cc6c644594437782109724f754b6a3d1d67f3140
steffhaa <steffen.haas@htwg-konstanz.de>
2024-01-18 16:41:51 +0100

    teste gamemodespec

--- src/test/scala/de/htwg/se/wordle/model/gamemodeComponent/gamemodespec.scala
+++ src/test/scala/de/htwg/se/wordle/model/gamemodeComponent/gamemodespec.scala
@@ -5,7 +5,7 @@ import org.scalatest.wordspec.AnyWordSpec
 class GamemodeSpec extends AnyWordSpec {
 
   "Gamemode1" should {
-    val wordObject = new Word() // Ersetzen Sie dies durch eine tatsächliche Implementierung von Word
+    val wordObject = new Word() //..
     val gamemode1Instance = gamemode1(wordObject)
 
     "correctly get the target word" in {
@@ -31,5 +31,60 @@ class GamemodeSpec extends AnyWordSpec {
     }
   }
 
-  // Ähnliche Tests für Gamemode2 und Gamemode3
+  "Gamemode2" should {
+    val wordObject = new Word() //...
+    val gamemode2Instance = gamemode2(wordObject)
+
+    "correctly get the target word" in {
+      assert(gamemode2Instance.getTargetword().size == 2)
+    }
+
+    "correctly get the limit" in {
+      assert(gamemode2Instance.getLimit() == 7)
+    }
+
+    "correctly get the word list" in {
+      assert(gamemode2Instance.getWordList().nonEmpty)
+    }
+
+    "allow setting a new target word" in {
+      gamemode2Instance.setTargetWord(Map(1 -> "testWord1", 2 -> "testWord2"))
+      assert(gamemode2Instance.getTargetword() == Map(1 -> "testWord1", 2 -> "testWord2"))
+    }
+
+    "allow setting a new limit" in {
+      gamemode2Instance.setLimit(9)
+      assert(gamemode2Instance.getLimit() == 9)
+    }
+  }
+
+  "Gamemode3" should {
+    val wordObject = new Word() //
+    val gamemode3Instance = gamemode3(wordObject)
+
+    "correctly get the target word" in {
+      assert(gamemode3Instance.getTargetword().size == 4)
+    }
+
+    "correctly get the limit" in {
+      assert(gamemode3Instance.getLimit() == 8)
+    }
+
+    "correctly get the word list" in {
+      assert(gamemode3Instance.getWordList().nonEmpty)
+    }
+
+    "allow setting a new target word" in {
+      gamemode3Instance.setTargetWord(Map(1 -> "word1", 2 -> "word2", 3 -> "word3", 4 -> "word4"))
+      assert(gamemode3Instance.getTargetword() == Map(1 -> "word1", 2 -> "word2", 3 -> "word3", 4 -> "word4"))
+    }
+
+    "allow setting a new limit" in {
+      gamemode3Instance.setLimit(11)
+      assert(gamemode3Instance.getLimit() == 11)
+    }
+  }
+
+
+
 }
