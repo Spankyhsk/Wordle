fb1a019f0c2e13e8de2cb7f3bda18cfdb1bbcea8
steffhaa <steffen.haas@htwg-konstanz.de>
2024-01-22 23:42:00 +0100

    test2 jasonspec

--- src/test/scala/de/htwg/se/wordle/model/FileIOComponent/FileIOJsonSpec.scala
+++ src/test/scala/de/htwg/se/wordle/model/FileIOComponent/FileIOJsonSpec.scala
@@ -6,7 +6,9 @@ import de.htwg.se.wordle.model.gamemechComponent.GameMech
 import de.htwg.se.wordle.model.gamemodeComponnent.gamemode
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
-import play.api.libs.json.JsObject
+import play.api.libs.json.{JsObject, Json}
+
+import java.io.{File, PrintWriter}
 
 class FileIOJsonSpec extends AnyWordSpec with Matchers {
   "FileIOJSON" when {
@@ -88,6 +90,132 @@ class FileIOJsonSpec extends AnyWordSpec with Matchers {
         loadedGame.getN() should be(originalGame.getN())
         loadedGame.getTargetword().keys should contain(1) // Check if key '1' is included in the target word map
         // Additional checks can be added here if needed
+      }
+    }
+
+    "loading a game with different game states" should {
+      "correctly change the game state based on the winningBoard size" in {
+        val fileIO = new FileIOJSON
+        val fileName = "game.json"
+
+        // Funktion zum Schreiben einer Test-JSON-Datei
+        def writeTestJson(jsonContent: JsObject): Unit = {
+          val pw = new PrintWriter(new File(fileName))
+          try {
+            pw.write(Json.prettyPrint(jsonContent))
+          } finally {
+            pw.close()
+          }
+        }
+
+        // Testdaten vorbereiten
+        val jsonWithWinningBoardSize1 = Json.obj("game" -> Json.obj("mech" -> Json.obj("winningboard" -> Json.toJson(Map(1 -> false)))))
+        val jsonWithWinningBoardSize2 = Json.obj("game" -> Json.obj("mech" -> Json.obj("winningboard" -> Json.toJson(Map(1 -> false, 2 -> false)))))
+        val jsonWithWinningBoardSize4 = Json.obj("game" -> Json.obj("mech" -> Json.obj("winningboard" -> Json.toJson(Map(1 -> false, 2 -> false, 3 -> false, 4 -> false)))))
+
+        // Testfälle durchführen
+        writeTestJson(jsonWithWinningBoardSize1)
+        val game1: GameInterface = new Game(new GameMech(), new gameboard(), gamemode(1))
+        fileIO.load(game1)
+        game1.getGamemode().toString should include("Wort1:")
+
+        writeTestJson(jsonWithWinningBoardSize2)
+        val game2: GameInterface = new Game(new GameMech(), new gameboard(), gamemode(1))
+        fileIO.load(game2)
+        game2.getGamemode().toString should include("Wort1:")
+
+        writeTestJson(jsonWithWinningBoardSize4)
+        val game3: GameInterface = new Game(new GameMech(), new gameboard(), gamemode(1))
+        fileIO.load(game3)
+        game3.getGamemode().toString should include("Wort1:")
+      }
+    }
+
+    "saving a game" should {
+      "correctly serialize the gameboard into JSON" in {
+        val game = new Game(new GameMech(), new gameboard(), gamemode(1))
+        val fileIO = new FileIOJSON
+
+        // Erstellen eines Gameboards mit spezifischen Daten
+        game.getGamefield().setMap(Map(1 -> Map(1 -> "A", 2 -> "B"), 2 -> Map(1 -> "C", 2 -> "D")))
+
+        val json = fileIO.gameToJson(game)
+
+        // Überprüfen, ob das JSON korrekt serialisiert wurde
+        (json \ "game" \ "board" \ "gameboard").as[Seq[JsObject]].size should be(2)
+        (json \\ "gamefield").map(_.as[Map[String, String]]) should contain allOf(Map("1" -> "A", "2" -> "B"), Map("1" -> "C", "2" -> "D"))
+      }
+    }
+
+    "loading a game" should {
+      /*"correctly deserialize the gameboard from JSON" in {
+        val fileIO = new FileIOJSON
+        val fileName = "game.json"
+
+        // Funktion zum Schreiben einer Test-JSON-Datei
+        def writeTestJson(jsonContent: JsObject): Unit = {
+          val pw = new PrintWriter(new File(fileName))
+          try {
+            pw.write(Json.prettyPrint(jsonContent))
+          } finally {
+            pw.close()
+          }
+        }
+
+        // Beispiel für JSON-Inhalt mit einem Gameboard
+        val jsonWithGameBoard = Json.obj(
+          "game" -> Json.obj(
+            "board" -> Json.obj(
+              "gameboard" -> Json.toJson(
+                Seq(
+                  Json.obj("key" -> 1, "gamefield" -> Map("1" -> "A", "2" -> "B")),
+                  Json.obj("key" -> 2, "gamefield" -> Map("1" -> "C", "2" -> "D"))
+                )
+              )
+            )
+          )
+        )
+
+        // Schreiben des JSON-Inhalts in eine Datei und Laden des Spiels
+        writeTestJson(jsonWithGameBoard)
+        val game: GameInterface = new Game(new GameMech(), new gameboard(), gamemode(1))
+        fileIO.load(game)
+
+        // Überprüfen, ob das Gameboard korrekt deserialisiert wurde
+        game.getGamefield().getMap()(1).getMap() should contain allOf(1 -> "A", 2 -> "B")
+        game.getGamefield().getMap()(2).getMap() should contain allOf(1 -> "C", 2 -> "D")
+      }*/
+
+      "set the correct game mode based on the targetWord size" in {
+        val fileIO = new FileIOJSON
+        val fileName = "game.json"
+
+        // Funktion zum Schreiben einer Test-JSON-Datei
+        def writeTestJson(jsonContent: JsObject): Unit = {
+          val pw = new PrintWriter(new File(fileName))
+          try {
+            pw.write(Json.prettyPrint(jsonContent))
+          } finally {
+            pw.close()
+          }
+        }
+
+        // Beispiel für JSON-Inhalt mit einem Zielwort
+        val jsonWithTargetWord = Json.obj(
+          "game" -> Json.obj(
+            "mode" -> Json.obj(
+              "TargetWord" -> Json.toJson(Map(1 -> "WORT", 2 -> "WORT2"))
+            )
+          )
+        )
+
+        // Schreiben des JSON-Inhalts in eine Datei und Laden des Spiels
+        writeTestJson(jsonWithTargetWord)
+        val game: GameInterface = new Game(new GameMech(), new gameboard(), gamemode(1))
+        fileIO.load(game)
+
+        // Überprüfen, ob der richtige Spielmodus basierend auf der Größe des Zielworts gesetzt wurde
+        game.getGamemode().toString should include("Wort1:")
       }
     }
 
